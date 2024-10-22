d8d0b1a5b474e30850b397fd5379a3d61fd94c5a
Luca <luca@luc.am>
2024-10-22 19:34:56 -0400

    back

--- cnc_calculator/domain/models/cutting_style.py
+++
@@ -1,5 +0,0 @@
-class CuttingStyle:
-    def __init__(self, name: str, woc_multiplier: tuple[float, float], doc_multiplier: tuple[float, float]):
-        self.name = name
-        self.woc_multiplier = woc_multiplier
-        self.doc_multiplier = doc_multiplier
